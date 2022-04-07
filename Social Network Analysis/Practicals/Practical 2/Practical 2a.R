c5cffd21b82aa17d094bf22155114f55995961a0
Miteshkonkar <42760922+Miteshkonkar@users.noreply.github.com>
2022-04-07 13:01:44 +0530

    Delete Social Network Analysis directory

--- Social Network Analysis/Practicals/Practical 2/Practical 2a.R
+++
@@ -1,8 +0,0 @@
-library(igraph)
-
-nodes<-read.csv("nodes.csv")
-head(nodes)
-
-
-links<-read.csv("edges.csv")
-head(links)
--- Social Network Analysis/Practicals/Practical 2/Practical 2b.R
+++
@@ -1,6 +0,0 @@
-net<-graph.data.frame(d=links,vertices=nodes,directed = T)
-m=as.matrix(net)
-get.adjacency(m)
-
-
-plot(net)
